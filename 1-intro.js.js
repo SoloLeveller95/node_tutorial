19f48325d0d2de48b121556b5827f7d168aa0030
Fathullah Harun <sololeveller@gmail.com>
2022-08-20 18:06:44 +0800

    First Commit

---
+++ 1-intro.js.js
@@ -0,0 +1,16 @@
+// NodeJS basics
+
+// Enter the commands in CMD
+// node -v >>> To check node version in your computer
+// node >>> TO enter REPL
+
+// Enter the commands in VScode Terminal
+// node <filename> >>> To run node in the terminal
+
+const amount = 6;
+
+if (amount < 12) {
+	console.log("Small number");
+} else {
+	console.log("Large number");
+}
